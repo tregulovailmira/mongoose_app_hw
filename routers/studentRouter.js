5b902652f40c3835161da7809524d4c568fdea13
Ilmira <tregulovailmira@gmail.com>
2020-12-25 16:45:13 +0200

    Create student router

--- routers/studentRouter.js
+++ routers/studentRouter.js
@@ -1,9 +1,18 @@
 const { Router } = require('express');
+const { validate } = require('../middleware');
+const { studentController } = require('../controllers');
 
 const studentRouter = new Router();
 
-studentRouter.route('./').post().get();
+studentRouter
+  .route('/')
+  .post(validate.createStudentValidate, studentController.createStudent)
+  .get(studentController.getAllStudents);
 
-studentRouter.route('./:studentId').get().patch().delete();
+studentRouter
+  .route('/:studentId')
+  .get(studentController.getStudent)
+  .patch(validate.updateStudentValidate, studentController.updateStudent)
+  .delete(studentController.deleteStudent);
 
 module.exports = studentRouter;
